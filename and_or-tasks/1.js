a96e901f39f6f65071f9f7084357c95596a884d3
Alexander Roshchin <alexander.roshchin85@gmail.com>
2020-06-21 21:59:43 +0300

    Add first task to use && and ||

---
+++ and_or-tasks/1.js
@@ -0,0 +1,11 @@
+const num = prompt('Введите число: ')
+let a = Number(num)
+
+/**
+ * Checks the number for compliance
+ */
+if (a > 0 && a < 5) {
+    alert('Верно')
+} else {
+    alert('Неверно')
+}
